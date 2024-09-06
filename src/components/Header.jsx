adc15a542eb6ca94336baa4c3f093e1c6c8c1328
Kalpesh <kalpeshdhoke7@gmail.com>
2024-09-06 15:04:57 +0530

    define frameworksDropDown component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import MenuIcon from "./MenuIcon";
 import { useState } from "react";
-// import PrimaryButton from "./PrimaryButton";
+import FrameworksDropdown from "./FrameworksDropdown";
 
 const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [showFrameworks, setShowFrameworks] = useState(false);
 
   const handleToggle = () => {
     setIsOpen(!isOpen);
@@ -13,9 +14,16 @@ const Header = () => {
     <header className="container mx-auto flex justify-between items-center py-6 px-4">
       <img src="/hyperchain.svg" alt="hyperchain logo" className="h-8" />
       <nav className="hidden md:block">
-        <ul className="flex gap-10 items-center">
+        <ul className="flex gap-10 items-center cursor-pointer">
           <li>Work</li>
-          <li>Frameworks</li>
+          <li
+            onMouseEnter={() => setShowFrameworks(true)}
+            onMouseLeave={() => setShowFrameworks(false)}
+            className="relative"
+          >
+            Frameworks
+            {showFrameworks && <FrameworksDropdown />}
+          </li>
           <li>Services</li>
           <li>Contact</li>
         </ul>
